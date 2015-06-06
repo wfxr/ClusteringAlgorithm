6193b16780a9f6eece7d070615489e1bc4d07621
wfxr <wfxrgm@gmail.com>
2015-06-06 16:08:39 +0800

    添加减法运算

--- ClusteringAlgorithm/ClusteringAlgorithm/Point.cs
+++ ClusteringAlgorithm/ClusteringAlgorithm/Point.cs
@@ -1,26 +1,16 @@
 using System;
 
 namespace ClusteringAlgorithm {
-    public class Point :IComparable<Point>{
+    public class Point : IComparable<Point> {
+        public double X;
+        public double Y;
+        public double Z;
+
         public Point(double x, double y, double z) {
             X = x;
             Y = y;
             Z = z;
         }
-
-        public static double Distance(Point p1, Point p2)
-            => Math.Sqrt((p1.X - p2.X)*(p1.X - p2.X) +
-                    (p1.Y - p2.Y)*(p1.Y - p2.Y) +
-                    (p1.Z - p2.Z)*(p1.Z - p2.Z));
-
-        public static Point operator +(Point p1, Point p2)
-            => new Point(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
-        public static Point operator /(Point p1, double div)
-            => new Point(p1.X/div, p1.Y/div, p1.Z/div);
-
-        public double X;
-        public double Y;
-        public double Z;
 
         public int CompareTo(Point other) {
             if (X > other.X)
@@ -40,5 +30,19 @@ namespace ClusteringAlgorithm {
 
             return 0;
         }
+
+        public static double Distance(Point p1, Point p2)
+            => Math.Sqrt((p1.X - p2.X)*(p1.X - p2.X) +
+                         (p1.Y - p2.Y)*(p1.Y - p2.Y) +
+                         (p1.Z - p2.Z)*(p1.Z - p2.Z));
+
+        public static Point operator +(Point p1, Point p2)
+            => new Point(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+
+        public static Point operator -(Point p1, Point p2)
+            => new Point(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+
+        public static Point operator /(Point p1, double div)
+            => new Point(p1.X/div, p1.Y/div, p1.Z/div);
     }
 }
