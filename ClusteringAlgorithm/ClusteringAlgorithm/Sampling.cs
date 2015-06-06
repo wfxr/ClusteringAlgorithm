e0961511278fe0b051b4276973508c32a257e150
wfxr <wfxrgm@gmail.com>
2015-06-06 13:48:56 +0800

    重构

--- ClusteringAlgorithm/ClusteringAlgorithm/Sampling.cs
+++ ClusteringAlgorithm/ClusteringAlgorithm/Sampling.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace ClusteringAlgorithm {
     public class Sampling<T> {
+        // ReSharper disable once StaticMemberInGenericType
         private static Random Rand { get; } = new Random();
 
         /// <summary>
