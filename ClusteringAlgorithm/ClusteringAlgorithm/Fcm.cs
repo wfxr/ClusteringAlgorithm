0b1e69de7b9e34b43cff2e0282bce5466a849f88
wfxr <wfxrgm@gmail.com>
2015-06-10 11:33:23 +0800

    重构完成

--- ClusteringAlgorithm/ClusteringAlgorithm/Fcm.cs
+++ ClusteringAlgorithm/ClusteringAlgorithm/Fcm.cs
@@ -35,18 +35,31 @@ namespace ClusteringAlgorithm {
             double min_impro = 1e-5) {
             ValidateArgument(c, expo, max_iter, min_impro);
 
-            // 创建隶属度矩阵并完成行标准化,行数为分类数目,列数为观察值数目
+            // 创建隶属度矩阵并执行行标准化
             var U = MatrixBuilder.Random(c, n).NormalizeRows(1.0);
 
-            // 创建中心矩阵,行数为分类数目,列数为观察值维数
+            // 创建中心矩阵
             var C = MatrixBuilder.Dense(c, d);
 
-            // 目标函数向量
+            // 创建目标函数向量
             var obj_fcn = VectorBuilder.Dense(max_iter);
 
             // 主循环
             for (var i = 0; i < max_iter; ++i) {
-                obj_fcn[i] = SetpFcm(ref U, out C, c, expo);
+                // 隶属度指数修正矩阵
+                var Um = ComputeUm(U, expo);
+
+                // 更新中心矩阵
+                C = ComputeCenter(Um);
+
+                // 距离矩阵
+                var dist = ComputeDistance(C);
+
+                // 计算目标函数值
+                obj_fcn[i] = ComputeObjectFunctionn(dist, Um);
+
+                // 更新隶属度矩阵
+                U = ComputeU(dist, expo);
 
                 // 改进程度小于指定值则结束循环
                 if (i > 1 && Math.Abs(obj_fcn[i] - obj_fcn[i - 1]) < min_impro) break;
@@ -54,27 +67,64 @@ namespace ClusteringAlgorithm {
             return new FcmResult(C, U, obj_fcn);
         }
 
-        private double SetpFcm(ref Matrix<double> U, out Matrix<double> C, int c, double expo) {
-            var mf = U.Clone().PointwisePower(expo);
+        /// <summary>
+        ///     计算对隶属度矩阵进行指数修正后的矩阵
+        /// </summary>
+        /// <param name="U">隶属度矩阵</param>
+        /// <param name="expo">指数</param>
+        /// <returns>修正后的矩阵</returns>
+        private Matrix<double> ComputeUm(Matrix<double> U, double expo) => U.PointwisePower(expo);
 
-            C = (mf*data).PointwiseDivide(mf.RowSums().ToColumnMatrix()*MatrixBuilder.Dense(1, d, 1));
+        /// <summary>
+        ///     计算分类中心
+        /// </summary>
+        /// <param name="mf">隶属度修正矩阵</param>
+        /// <returns>中心矩阵</returns>
+        private Matrix<double> ComputeCenter(Matrix<double> mf)
+            => (mf*data).PointwiseDivide(mf.RowSums().ToColumnMatrix()*MatrixBuilder.Dense(1, d, 1));
 
-            var Dis = MatrixBuilder.Dense(c, n);
-            for (var i = 0; i < c; ++i)
+        /// <summary>
+        ///     计算每个观测值到各分类中心的距离
+        /// </summary>
+        /// <param name="C">分类中心矩阵</param>
+        /// <returns>距离矩阵</returns>
+        private Matrix<double> ComputeDistance(Matrix<double> C) {
+            var dist = MatrixBuilder.Dense(C.RowCount, n);
+            for (var i = 0; i < C.RowCount; ++i)
                 for (var j = 0; j < n; ++j)
-                    Dis[i, j] = Distance.Euclidean(data.Row(j), C.Row(i));
-
-            // 计算目标函数值
-            var obj_fcn = Dis.PointwisePower(2).PointwiseMultiply(mf).RowSums().Sum();
-
-            // 更新隶属度矩阵
-            var tmp = Dis.PointwisePower(-2/(expo - 1));
-            U = tmp.PointwiseDivide(MatrixBuilder.Dense(c, 1, 1)*tmp.ColumnSums().ToRowMatrix());
-
-            return obj_fcn;
+                    dist[i, j] = Distance.Euclidean(data.Row(j), C.Row(i));
+            return dist;
         }
 
+        /// <summary>
+        ///     计算目标函数值
+        /// </summary>
+        /// <param name="dist">距离矩阵</param>
+        /// <param name="mf">隶属度修正矩阵</param>
+        /// <returns>目标函数值</returns>
+        private double ComputeObjectFunctionn(Matrix<double> dist, Matrix<double> mf)
+            => dist.PointwisePower(2).PointwiseMultiply(mf).RowSums().Sum();
 
+        /// <summary>
+        ///     计算隶属度
+        /// </summary>
+        /// <param name="dist">距离矩阵</param>
+        /// <param name="expo">指数</param>
+        /// <returns>隶属度矩阵</returns>
+        private Matrix<double> ComputeU(Matrix<double> dist, double expo) {
+            var tmp = dist.PointwisePower(-2/(expo - 1));
+            return
+                tmp.PointwiseDivide(MatrixBuilder.Dense(dist.RowCount, 1, 1)*
+                                    tmp.ColumnSums().ToRowMatrix());
+        }
+
+        /// <summary>
+        ///     验证参数是否正确
+        /// </summary>
+        /// <param name="c">分类数目</param>
+        /// <param name="expo">指数</param>
+        /// <param name="max_iter">结束条件:最大迭代次数</param>
+        /// <param name="min_impro">结束条件:目标函数最小改进值</param>
         private void ValidateArgument(int c, double expo, int max_iter, double min_impro) {
             if (c > n)
                 throw new ArgumentException(
