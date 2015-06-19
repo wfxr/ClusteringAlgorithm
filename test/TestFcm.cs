9cd755fd572dee9f66adcfca50db75e531d89d99
wfxr <wfxrgm@gmail.com>
2015-06-19 16:25:10 +0800

    修复错误

--- test/TestFcm.cs
+++ test/TestFcm.cs
@@ -12,8 +12,8 @@ namespace UnitTest {
             var data = DenseMatrix.OfArray(new double[,] {
                 {1, 2, 3, 7, 8, 9, 13, 14, 15, 100, 120, 130}
             }).Transpose();
-            var km = new Kmeans(data);
-            var result = km.Clustering(k);
+            var km = new Fcm(data);
+            var result = km.Cluster(k);
             Assert.Equal(result.Center.RowCount, k);
             Assert.Equal(result.UV.Count(), data.RowCount);
          }
